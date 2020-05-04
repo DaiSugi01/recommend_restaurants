3dd8c1c8fc1d3b36a454fe0d53005479a5f9b320
Daisugi <volble124@gmail.com>
2020-05-04 21:45:31 +0900

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='recommend_restaurants',
+    version='1.0.0',
+    packages=['roboter', 'roboter.view', 'roboter.models', 'roboter.controller'],
+    url='',
+    license='Free',
+    author='sugiharadaiki',
+    author_email='',
+    description='recommend_restaurants package'
+)
