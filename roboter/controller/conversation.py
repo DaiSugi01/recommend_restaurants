b95ee06371d033b92db41a40120bce0e221fa8a8
Daisugi <volble124@gmail.com>
2020-05-05 02:01:39 +0900

    modify

--- roboter/controller/conversation.py
+++ roboter/controller/conversation.py
@@ -5,6 +5,7 @@ def talk_about_restaurant():
     restaurant_robot = robot.RestaurantRobot()
     user = users.Users()
     user.name = restaurant_robot.hello()
+    restaurant_robot.ask_preferable_or_not()
     restaurant_robot.ask_preferable_restaurant(user.name)
     restaurant_robot.say_good_bye(user.name)
 
--- roboter/models/robot.py
+++ roboter/models/robot.py
@@ -39,45 +39,48 @@ class RestaurantRobot(Robot):
         super().__init__()
         self.ranking_csv = ranking.RankingCsv()
 
+    def ask_preferable_or_not(self):
+        """ Ask some restaurant is preferable or not for user"""
+        if not(os.path.exists(self.ranking_csv.csv_filename)):
+            return
+
+        d_restaurant = self.ranking_csv.read_csv()
+        d_restaurant = sorted(d_restaurant.items(), key=lambda x: x[1], reverse=True)
+
+        # Show the recommend restaurants
+        isYes = False
+        for t_restaurant in d_restaurant:
+            restaurant_name, v = t_restaurant
+            contents = console.get_template('my_recommend_restaurant.txt')
+            yes_or_no = input(colored(contents.substitute(restaurant=restaurant_name), 'green')).capitalize()
+
+            while True:
+                if yes_or_no == 'Yes' or yes_or_no == 'Y':
+                    self.ranking_csv.add_restaurant(restaurant_name)
+                    isYes = True
+                    break
+                elif yes_or_no == 'No' or yes_or_no == 'N':
+                    break
+                else:
+                    print(colored('Yes/Noを入力してください', 'blue'))
+                    print(colored('You should input Yes/No', 'blue'))
+                    yes_or_no = input().capitalize()
+
+            if isYes:
+                break
+
     def ask_preferable_restaurant(self, user_name):
         """ Ask preferable restaurant to user
         :param
             user_name: user name
         """
-        if os.path.exists(self.ranking_csv.csv_filename):
-            d_restaurant = self.ranking_csv.read_csv()
-            d_restaurant = sorted(d_restaurant.items(), key=lambda x: x[1], reverse=True)
-
-            # Show the recommend restaurants
-            isYes = False
-            for t_restaurant in d_restaurant:
-                restaurant_name, v = t_restaurant
-                contents = console.get_template('my_recommend_restaurant.txt')
-                yes_or_no = input(colored(contents.substitute(restaurant=restaurant_name), 'green')).capitalize()
-
-                while True:
-                    if yes_or_no == 'Yes' or yes_or_no == 'Y':
-                        self.ranking_csv.add_restaurant(restaurant_name)
-                        isYes = True
-                        break
-                    elif yes_or_no == 'No' or yes_or_no == 'N':
-                        break
-                    else:
-                        print(colored('Yes/Noを入力してください', 'blue'))
-                        print(colored('You should input Yes/No', 'blue'))
-                        yes_or_no = input().capitalize()
-
-                if isYes:
-                    break
-
-        # Show the template for asking recommend restaurant
         contents = console.get_template('which_restaurant.txt')
         restaurant_name = input(colored(contents.substitute(name=user_name), 'green'))
         while True:
             if restaurant_name:
                 if ' ' in restaurant_name:
-                    before_soret_list = restaurant_name.split(' ')
-                    sorted_list = map(lambda s: s.capitalize(), before_soret_list)
+                    before_sort_list = restaurant_name.split(' ')
+                    sorted_list = map(lambda s: s.capitalize(), before_sort_list)
                     restaurant_name = ' '.join(sorted_list)
                 else:
                     restaurant_name = restaurant_name.capitalize()
